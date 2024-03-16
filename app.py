e8fc1e90d288796f088c24e24b34f1d3b949d7b2
Koujianing233 <144105672+Koujianing233@users.noreply.github.com>
2024-03-16 17:10:21 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,58 +1,78 @@
-import streamlit as st
-from src.inference import get_prediction
+import streamlit as st 
+# ML Pkg
+# from sklearn.externals import joblib
+import joblib
+import time
+from PIL import Image
 
-#Initialise session state variable
-if 'input_features' not in st.session_state:
-    st.session_state['input_features'] = {}
+# load Vectorizer For Gender Prediction
+gender_vectorizer = open("models/gender_vectorizer.pkl","rb")
+gender_cv = joblib.load(gender_vectorizer)
 
-def app_sidebar():
-    st.sidebar.header('Loan Details')
-    emp_length_options = ['< 1 year','1 year','2 years','3 years','4 years','5 years',
-                          '6 years','7 years','8 years','9 years','10+ years']
-    emp_length = st.sidebar.selectbox("Employment Length", emp_length_options)
-    int_rate = st.sidebar.slider('Loan Interest Rate', 5, 40, 10, 1)
-    annual_inc = st.sidebar.text_input("Annual Income '000s", placeholder="in '000s")
-    fico_range_high = st.sidebar.slider('FICO Upper Boundary', 600, 800, 700, 50)
-    loan_amnt = st.sidebar.text_input('Loan Amount')
-    def get_input_features():
-        input_features = {'emp_length': emp_length,
-                          'int_rate': int_rate,
-                          'annual_inc': int(annual_inc)*1000,
-                          'fico_range_high': fico_range_high,
-                          'loan_amnt': int(loan_amnt)
-                         }
-        return input_features
-    sdb_col1, sdb_col2 = st.sidebar.columns(2)
-    with sdb_col1:
-        predict_button = st.sidebar.button("Assess", key="predict")
-    with sdb_col2:
-        reset_button = st.sidebar.button("Reset", key="clear")
-    if predict_button:
-        st.session_state['input_features'] = get_input_features()
-    if reset_button:
-        st.session_state['input_features'] = {}
-    return None
+# load Model For Gender Prediction
+gender_nv_model = open("models/naivebayesgendermodel.pkl","rb")
+gender_clf = joblib.load(gender_nv_model)
 
-def app_body():
-    title = '<p style="font-family:arial, sans-serif; color:Black; font-size: 40px;"><b> Welcome to DSSI Loan Assessment</b></p>'
-    st.markdown(title, unsafe_allow_html=True)
-    default_msg = '**System assessment says:** {}'
-    if st.session_state['input_features']:
-        assessment = get_prediction(emp_length=st.session_state['input_features']['emp_length'],
-                                    int_rate=st.session_state['input_features']['int_rate'],
-                                    annual_inc=st.session_state['input_features']['annual_inc'],
-                                    fico_range_high=st.session_state['input_features']['fico_range_high'],
-                                    loan_amnt=st.session_state['input_features']['loan_amnt'])
-        if assessment.lower() == 'yes':
-            st.success(default_msg.format('Approved'))
-        else:
-            st.warning(default_msg.format('Rejected'))
-    return None
+
+# Prediction
+def predict_gender(data):
+	vect = gender_cv.transform(data).toarray()
+	result = gender_clf.predict(vect)
+	return result
+
+# Load CSS File
+def load_css(file_name):
+    with open(file_name) as f:
+        st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+
+def load_icon(icon_name):
+    st.markdown('<i class="material-icons">{}</i>'.format(icon_name), unsafe_allow_html=True)
+
+# Images
+def load_images(file_name):
+	img = Image.open(file_name)
+	return st.image(img,width=300)
+
 
 def main():
-    app_sidebar()
-    app_body()
-    return None
+	"""Gender Classifier App
+	Using Machine Learning and Streamlit
 
-if __name__ == "__main__":
-    main()
+	"""
+
+	st.title("Gender Classifier")
+	html_temp = """
+	<div style="background-color:tomato;padding:10px">
+	<h2 style="color:white;text-align:center;">Streamlit ML App </h2>
+	</div>
+
+	"""
+	st.markdown(html_temp,unsafe_allow_html=True)
+	load_css('icon.css')
+	load_icon('people')
+
+	name = st.text_input("Enter Name","Type Here")
+	if st.button("Predict"):
+		result = predict_gender([name])
+		if result[0] == 0:
+			prediction = 'Female'
+			c_img = 'female.png'
+		else:
+			result[0] == 1
+			prediction = 'Male'
+			c_img = 'male.png'
+
+		st.success('Name: {} was classified as {}'.format(name.title(),prediction))
+		load_images(c_img)
+
+
+	if st.button("About"):
+
+		st.text("Jesus Saves @JCharisTech")
+		st.text("By Jesse E.Agbe(JCharis)")
+		st.text("Built with Streamlit")
+
+
+
+if __name__ == '__main__':
+	main()
